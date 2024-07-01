97be0147017d80c8b561a7d141d2ad395d75ebf3
Keerthi.Gundureddy <gundureddykeerthi123@gmail.com>
2024-07-01 17:37:32 +0530

    day3 assignment commit

--- python/basics/day3_Assignment.py
+++ python/basics/day3_Assignment.py
@@ -10,7 +10,13 @@ sum_first_n_integers(5) should return 15
 
 def sum_first_n_integers(n):
     # Your code here
+    sum=n*(n+1)/2
+    return sum
     pass
+n=5
+print(sum_first_n_integers(n))
+
+
 
 
 """
@@ -25,8 +31,14 @@ convert_height_to_cm(5, 7) should return 170.18
 
 def convert_height_to_cm(feet, inches):
     # Your code here
-    pass
+   Centimeters = (Feet * 30.48) + (Inches * 2.54)
+   return Centimeters
 
+pass
+
+Feet=5
+Inches=7
+print(convert_height_to_cm(Feet,Inches))
 
 """
 Problem 60:
@@ -40,8 +52,11 @@ calculate_hypotenuse(3, 4) should return 5.0
 
 def calculate_hypotenuse(base, height):
     # Your code here
+    Hypotenuse = (base ** 2 + height ** 2)**0.5
+    return Hypotenuse
     pass
-
+base, height=3,4
+print(calculate_hypotenuse(base,height))
 
 """
 Problem 61:
@@ -55,8 +70,13 @@ convert_distance(5280) should return (63360, 1760, 1.0)
 
 def convert_distance(feet):
     # Your code here
+    distance_in_inches=foot*12*5280
+    distance_in_yards=round(foot*0.33333*5280)
+    distance_in_miles=round(foot*0.000189394*5280,1)
+    return distance_in_inches,distance_in_yards,distance_in_miles
     pass
-
+foot=1
+print(convert_distance(foot))
 
 """
 Problem 66:
@@ -70,8 +90,12 @@ calculate_bmi(70, 1.75) should return 22.86
 
 def calculate_bmi(weight, height):
     # Your code here
-    pass
+    BMI =round( weight / height ** 2, 2)
 
+    return BMI
+    pass
+weight,height=70,1.75
+print(calculate_bmi(weight, height))
 
 """
 Problem 67:
@@ -88,9 +112,15 @@ convert_pressure(100) should return (14.5038, 750.062, 0.986923)
 
 def convert_pressure(kpa):
     # Your code here
+    pressure_in_psi=0.145038*kpa*pressure
+    pressure_in_mmHg=7.50062*kpa*pressure
+    pressure_in_atm = 0.00986923 * kpa*pressure
+    return pressure_in_psi,pressure_in_mmHg,pressure_in_atm
     pass
 
-
+pressure=100
+kpa=1
+print(convert_pressure(kpa))
 """
 Problem 68:
 Write a Python program to calculate the sum of digits of a number.
@@ -102,4 +132,9 @@ sum_of_digits(1234) should return 10
 
 def sum_of_digits(number):
     # Your code here
+    sum=int(num1[0])+int(num1[1])+int(num1[2])+int(num1[3])
+    return sum
     pass
+num=1234
+num1=str(1234)
+print(sum_of_digits(num1))
