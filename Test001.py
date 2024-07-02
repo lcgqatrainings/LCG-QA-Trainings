c011102d6db2a5a7c433b02092bb5a2f61e615be
Tejasvi.Kashan <tejasvikashan0902@gmail.com>
2024-07-02 12:56:06 +0530

    test123

---
+++ Test001.py
@@ -0,0 +1,23 @@
+def say_hello(name):
+    print(f'Hello {name}')
+say_hello('tejasvi')
+say_hello('MB')
+
+def my_age(age):
+    print(f'my age is {age}')
+my_age(18)
+
+def calculate_area_of_circle(radius):
+    print(f'radius of circle is {radius}')
+area_of_circle=pi_value*radius**2
+pi_value=3.17
+calculate_area_of_circle(0.62 )
+
+n1=input("Enter first number:")
+n2=input("Enter second number:")
+print(10)
+
+
+
+
+
