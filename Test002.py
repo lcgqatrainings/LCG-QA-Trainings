0ebfee8867f0cf1ef6d9517c4409718833d503eb
Tejasvi.Kashan <tejasvikashan0902@gmail.com>
2024-07-02 12:19:14 +0530

    test12

---
+++ Test002.py
@@ -0,0 +1,14 @@
+n1=int((input("Enter first number:"))
+n2=int(input("Enter second number:"))
+
+n1=(input("Enter first number:"))
+n2=(input("Enter second number:"))
+
+print(type(n1))
+print(type(n2))
+
+print(n1+n2)
+
+
+# print(int(n1)+int(n2))
+
