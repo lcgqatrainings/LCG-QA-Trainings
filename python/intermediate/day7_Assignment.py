fe883dabb5d4a26a09d11a48ef4e4ad335ac0b9a
vijay_p <vijayharsha1212@gmail.com>
2024-07-06 12:58:09 +0530

    practice-day-7

--- python/intermediate/day7_Assignment.py
+++ python/intermediate/day7_Assignment.py
@@ -8,7 +8,10 @@ flatten_list([1, [2, 3], [4, [5, 6]]]) should return [1, 2, 3, 4, 5, 6]
 
 
 def flatten_list(nested_list):
-    pass
+    if len(nested_list) > 5:
+        pass
+    else:
+        pass
 
 
 """
